7440f1b4f1eb3cbb70e7c88c3140cd5ec91ebfbe
git-an756 <kamlesh.adv100@gmail.com>
2022-03-14 11:22:22 +0530

    first commit

---
+++ Fibo.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class Fibo {
+
+	public static void main(String[] args) {
+		int a=1, b=1, range, c, sum=0;
+		range=1;
+		   while( range<=20 )
+		   {
+		     System.out.print(a +" ");
+		      sum =sum+ a;
+		      c = a + b;
+		      a = b;
+		      b = c;
+		      range++;
+		   }
+		   
+		System.out.println("sum of 20 terms: "+ sum);
+	}
+
+}
+
